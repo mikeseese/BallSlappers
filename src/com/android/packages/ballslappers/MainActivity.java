35be28c2b5ef88d822ebef2ceae4c57e9d2bfd54
Taylor Peoples <peoples.taylor@gmail.com>
2012-10-07 14:24:49 -0400

    Add MainActivity.java, proof of concept for maps/ball

---
+++ src/com/android/packages/ballslappers/MainActivity.java
@@ -0,0 +1,175 @@
+package com.example.andengine.sample;
+
+import static org.andengine.extension.physics.box2d.util.constants.PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT;
+
+import java.util.Random;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.handler.physics.PhysicsHandler;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.primitive.Ellipse;
+import org.andengine.entity.primitive.Line;
+import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.Scene.IOnSceneTouchListener;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.shape.IAreaShape;
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.extension.physics.box2d.util.Vector2Pool;
+import org.andengine.input.sensor.acceleration.AccelerationData;
+import org.andengine.input.sensor.acceleration.IAccelerationListener;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.debug.Debug;
+
+import android.hardware.SensorManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+public class MainActivity extends SimpleBaseGameActivity implements IOnSceneTouchListener {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final int CAMERA_WIDTH = 800;
+	private static final int CAMERA_HEIGHT = 480;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	private Scene mScene;
+
+	private PhysicsWorld mPhysicsWorld;
+	
+	private Body ballBody;
+	private Random randomNumGen = new Random();
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		Toast.makeText(this, "Here we go...", Toast.LENGTH_LONG).show();
+
+		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), camera);
+	}
+
+	@Override   
+	public void onCreateResources() {
+
+	}
+
+	@Override
+	public Scene onCreateScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+
+		this.mScene = new Scene();
+		this.mScene.setBackground(new Background(0, 0, 0));
+		this.mScene.setOnSceneTouchListener(this);
+
+		this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, 0), false);
+
+		final VertexBufferObjectManager vertexBufferObjectManager = this.getVertexBufferObjectManager();
+		final Rectangle ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2, vertexBufferObjectManager);
+		final Rectangle roof = new Rectangle(0, 0, CAMERA_WIDTH, 2, vertexBufferObjectManager);
+		final Rectangle left = new Rectangle(0, 0, 2, CAMERA_HEIGHT, vertexBufferObjectManager);
+		final Rectangle right = new Rectangle(CAMERA_WIDTH - 2, 0, 2, CAMERA_HEIGHT, vertexBufferObjectManager);
+		final Line randomLine = new Line(0, 0, 400, 300, vertexBufferObjectManager);
+		
+		final Rectangle ballShape = new Rectangle(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 2, 10, 10, vertexBufferObjectManager);
+		
+		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 1.0f, 0.0f);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, ground, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, roof, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, left, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, right, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createLineBody(this.mPhysicsWorld, randomLine, wallFixtureDef);
+		
+		final FixtureDef ballDef = PhysicsFactory.createFixtureDef(0, 1.0f, 0.0f);
+		ballBody = PhysicsFactory.createBoxBody(this.mPhysicsWorld, ballShape, BodyType.DynamicBody, ballDef);		
+		
+		this.mScene.attachChild(ground);
+		this.mScene.attachChild(roof);
+		this.mScene.attachChild(left);
+		this.mScene.attachChild(right);
+		this.mScene.attachChild(randomLine);
+	
+		this.mScene.attachChild(ballShape);
+		mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(ballShape, ballBody));
+		
+		this.mScene.registerUpdateHandler(this.mPhysicsWorld);
+
+		ballBody.setLinearVelocity(randomNumGen.nextInt(), randomNumGen.nextInt());
+		
+		return this.mScene;
+	}
+
+	@Override
+	public boolean onSceneTouchEvent(final Scene pScene, final TouchEvent pSceneTouchEvent) {
+		if(this.mPhysicsWorld != null) {
+			if(pSceneTouchEvent.isActionDown()) {
+				// determine if the paddle is being pressed, then deal with that
+				
+				// for now let's move the ball randomly when the screen is pressed
+				move_ball();
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public void onResumeGame() {
+		super.onResumeGame();
+	}
+
+	@Override
+	public void onPauseGame() {
+		super.onPauseGame();
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	private void move_ball() {
+		ballBody.setLinearVelocity(randomNumGen.nextInt(), randomNumGen.nextInt());
+		Log.v("ballBodyVelocity", ballBody.getLinearVelocity().toString());
+	}
+	
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===================== ======================================
+	
+}
